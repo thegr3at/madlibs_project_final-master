7012c596e52d544a9e5e59982d14e194c6ce99e3
thegr3at <ahmedjasem400>
2022-08-15 17:05:55 -0700

    Made changes to the program.
    Added 4 functions that will show menu, play game, make madlib, and print madlib.
    + The show menu will ask userinput for a choice.
    + The playgame welcomes the user.
    + The make madlib opens the madlib files and rewrite it in a list with new noun, verb, or adjective.
    + Finally print madlib will print out the results of the filled madlib.

--- madlibs/madlibs.py
+++ madlibs/madlibs.py
@@ -7,39 +7,47 @@ File madlibs.py
 """
 
 def showMenu():
-      fileName = input("Please enter a file name: ")
-      fileName = "files/" + fileName
-      schoolMadlib = open(fileName, "r")
-      reader = schoolMadlib.readlines()
-      makeMadlib(reader)
-
-def playGame():
     while True:
-        print(f"Welcome to my Madlibs Console Game!")
+
         userChoice = input(f"\nPlease make a choice from the menu below."
         f"\n1: Make a madlib"                                            
         f"\n2: Exit\n")
         if userChoice == "2":
               break
         else:
-             showMenu()
+             makeMadlib()
 
 
-def makeMadlib(reader):
-  missingValues = []
-  replacmentValues = []
-  for line in reader:
-        words = line.split()
-        [missingValues.append(word) for word in words if "<" and ">" in word]
+def playGame():
+    print(f"Welcome to my Madlibs Console Game!")
+    showMenu()
 
 
-  for value in missingValues:
-        replacment = input(f"Enter one {value}: ")
-        replacmentValues.append(replacment)
+def makeMadlib():
+    print(f"Files you can open:"
+          f"\njungle.madlib"
+          f"\nschool.madlib"
+          f"\nzoo.madlib")
+    fileName = input("\nPlease select one of the files above: ")
+    fileName = "files/" + fileName
+    fileOpen = open(fileName, "r")
+    reader = fileOpen.readlines()
+    lines = []
 
+    for line in reader:
+        if "<noun>" in line:
+            line = line.replace("<noun>", input("Enter one noun: "))
+        if "<verb>" in line:
+            line = line.replace("<verb>", input("Enter one verb: "))
+        if "<adjective>" in line:
+            line = line.replace("<adjective>", input("Enter one adjective: "))
+        lines.append(line)
+
+    printMadlib(lines)
 
 def printMadlib(lines):
-    print("print the madlib here...")
+    for line in lines:
+        print(line)
 
 playGame()
 
